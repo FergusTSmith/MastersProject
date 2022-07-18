c39be2d70e811a71ba4912f628e57e4d9fd6cf1f
GooseAye <fergustsmith@gmail.com>
2022-07-18 12:58:43 +0100

    Minor fixes

--- api/server.js
+++ api/server.js
@@ -81,7 +81,7 @@ io.on('connection', (socket) => {
                 socket.emit('lobbySuccess', lobbyID);
                 console.log("User has successfully joined the lobby " + lobbyID);
                 availableLobbies[i].addUser(UserID);
-                socket.nsp.to(availableLobbies[i]).emit('updateUsers', (availableLobbies[i].lobbyUsers, availableLobbies[i].LobbyID))
+                socket.nsp.to(availableLobbies[i]).emit('updateUsers', [availableLobbies[i].lobbyUsers, availableLobbies[i].LobbyID])
                 return;
             }else{
                 continue;
