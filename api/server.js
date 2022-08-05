02740736e03e37335e509c83e2453b53f81ec9eb
GooseAye <fergustsmith@gmail.com>
2022-08-05 12:10:04 +0100

    More server fixes

--- api/server.js
+++ api/server.js
@@ -428,7 +428,7 @@ db.sequelize.sync().then((req) => {
             for(var i = 0; i < availableLobbies.length; i++){
                 console.log("looking for lobby to rejoin..." + lobbyID)
                 if(availableLobbies[i].LobbyID === lobbyID){
-                    socket.nsp.to('RejoinGame', GameMode, timer, LobbyUsers)
+                    socket.nsp.to(availableLobbies[i]).emit('RejoinGame', GameMode, timer, LobbyUsers)
                     console.log("Found lobby and rejoining game");
                 }
             }
